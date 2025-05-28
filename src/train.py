d2ac6127a778b0ac0283047233266f62f7b2f845
rghmd00 <rm1801@fayoum.edu.eg>
2025-05-29 01:41:22 +0300

    hydra2 commit

--- src/train.py
+++ src/train.py
@@ -6,8 +6,8 @@ def train_model(df_train,cfg):
     for col in df_train.select_dtypes(include='object').columns:
         df_train[col] = df_train[col].astype('category')
 
-    X = df_train.drop(columns='price')
-    y = df_train['price']
+    X = df_train.drop(columns=cfg.target_column)
+    y = df_train[cfg.target_column]
 
     model = lgb.LGBMRegressor()
     model.fit(X, y)
