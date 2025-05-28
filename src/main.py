b2cec8406f97e8e7a497a492ffdcb5d04ae132ed
rghmd00 <rm1801@fayoum.edu.eg>
2025-05-29 01:20:59 +0300

    hydra1 commit

--- src/main.py
+++ src/main.py
@@ -2,11 +2,12 @@
 from src.data_load import load_csv
 from src.preprocess import wrangle
 from src.train import train_model
+import hydra
+from omegaconf import DictConfig
 
-
-def main():
-    # Step 1: Load data
-    train_raw = load_csv("data/raw/train.csv")
+@hydra.main(config_path="../configs", config_name="config", version_base=None)
+def main(cfg: DictConfig):
+    train_raw = load_csv(cfg.data.path)
     preprocessed_df = wrangle(train_raw)
     df_target = preprocessed_df['price']
     predictor = train_model(preprocessed_df)
